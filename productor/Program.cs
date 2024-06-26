dae315ffb41b41c592e72e718420723697334736
Samuel Colmenares <samuelcolmenares@MacBook-Pro-de-Samuel.local>
2024-06-25 21:23:53 -0500

    Add files lab

---
+++ productor/Program.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Messaging.ServiceBus;
+
+string conn = "<<connectionString>>";
+string queueName = "<<Queue Name>>";//"az204-queue";
+
+ServiceBusClient client;
+ServiceBusSender sender;
+
+client = new ServiceBusClient(conn);
+sender = client.CreateSender(queueName);
+
+using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+for (int i = 1; i <=3; i++)
+{
+    if(!messageBatch.TryAddMessage(new ServiceBusMessage($"Mensaje productor enviado {i}"))){
+        throw new Exception($"Exception en mensaje {i} ha ocurrido.");
+    }
+}
+
+try
+{
+    await sender.SendMessagesAsync(messageBatch);
+    Console.WriteLine("Se envió mensajes a la cola");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Se produjo error enviando: {ex.Message}");
+}
+finally{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+Console.WriteLine("Finaliza..");
+Console.ReadKey();
---
+++ receptor/Program.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Messaging.ServiceBus;
+
+tring conn = "<<connectionString>>";
+string queueName = "<<Queue Name>>";//"az204-queue";
+
+ServiceBusClient client;
+ServiceBusProcessor processor;
+
+client = new ServiceBusClient(conn);
+processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+    Console.WriteLine("Iniciando proceso de lectura ... espere un momento");
+    Console.ReadKey();
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($" Recibido:\t{body}");
+
+    await args.CompleteMessageAsync(args.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs args)  
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
